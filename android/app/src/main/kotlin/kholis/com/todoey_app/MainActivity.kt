87294017f386004cf10e1e24f465f2fcc2cb3b29
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-05-11 20:09:51 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/kholis/com/todoey_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package kholis.com.todoey_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TaksScreen(),
+    );
+  }
+}
+
